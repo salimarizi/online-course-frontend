0abfea299f19c672bad305987129e6ddedcf0d68
salimarizi <salim9asmp1pdl@gmail.com>
2023-02-18 20:07:39 +0700

    fix unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,12 @@
+import React from "react";
+import { Provider } from "react-redux";
+import store from "./store";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { render, screen } from "@testing-library/react";
+
 import App from "./App";
-import Banner from "./components/home/Banner";
+import HomePage from "./pages/Home";
+import ExercisePage from "./pages/Exercise";
 
 test("renders learn react link", () => {
   render(<App />);
@@ -9,7 +15,29 @@ test("renders learn react link", () => {
 });
 
 test("renders component in home page", () => {
-  render(<Banner />);
+  render(
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Router>
+    </Provider>
+  );
   const pythonTitle = screen.getByText("Python");
   expect(pythonTitle).toBeInTheDocument();
+});
+
+test("renders component in exercise", () => {
+  render(
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<ExercisePage />} />
+        </Routes>
+      </Router>
+    </Provider>
+  );
+  const runButton = screen.getByText("Run");
+  expect(runButton).toBeInTheDocument();
 });
