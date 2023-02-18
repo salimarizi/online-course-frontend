1a8c58b8ee8f9a17d72bae40517c4aaf30217de2
salimarizi <salim9asmp1pdl@gmail.com>
2023-02-18 19:27:37 +0700

    add unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import Banner from "./components/home/Banner";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("renders component in home page", () => {
+  render(<Banner />);
+  const pythonTitle = screen.getByText("Python");
+  expect(pythonTitle).toBeInTheDocument();
 });
